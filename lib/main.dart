8bf3ca5e7eb596e2d2d841f8d891e7894a5adccc
Mohamed Hagag <145903587+Mohamed-7agag@users.noreply.github.com>
2024-08-17 18:08:49 +0300

    Rename splashscreen import path to views directory
    Updated the import path for the splashscreen from the screens directory
    to the views directory in main.dart. This change ensures that the
    correct file structure is followed and the application can locate the
    splashscreen file without issues.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors,
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_ahades_40/screens/splashscreen.dart';
+import 'package:flutter_application_ahades_40/views/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
